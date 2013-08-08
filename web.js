f3008c743bf3eb40cce6c3e5ac5d7ca2f27e3144
startupac <adicirstei@hotmail.com>
2013-08-08 14:59:19 +0300

    just in case

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.createReadStream(__dirname + '/index.html')
+  fs.createReadStream(__dirname + '/assets/index.html')
     .pipe(response);
 });
 
